9afe9f6926c9d7e3e1f3d19496dbef4388f48534
gdavis-21 <gdavis19@uab.edu>
2022-06-14 14:23:01 -0500

    Initial Commit

---
+++ Learning-App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Learning-App
+//
+//  Created by Grant Davis on 6/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Learning-App/Learning_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Learning_AppApp.swift
+//  Learning-App
+//
+//  Created by Grant Davis on 6/14/22.
+//
+
+import SwiftUI
+
+@main
+struct Learning_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
